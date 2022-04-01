539243eba5900a05b8bbee26769cc088cb1aeca4
gabsrethink <gabriel.melo@rethink.dev>
2022-04-01 18:07:56 -0300

    feat: Complete Blog

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const blogs_div = document.querySelector(".blogs");
+
+const renderPosts = async () => {
+  const url = "http://localhost:3000/posts";
+  const res = fetch(url).then((response) => response.json());
+  const posts = await res;
+
+  let template = "";
+
+  posts.forEach((post) => {
+    template += `
+    <div class="post">
+  <h2>${post.title}</h2>
+  <p>
+    <small>${post.likes} Likes</small>
+  </p>
+  <p>${post.body.length > 180 ? post.body.slice(0, 180) + "..." : post.body}</p>
+  <a href="/details.html?id=${post.id}">Read More</a>
+</div>`;
+  });
+
+  blogs_div.innerHTML = template;
+};
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
