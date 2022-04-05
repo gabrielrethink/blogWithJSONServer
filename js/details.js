c5b1d1209db69bd4f3fcfa8f1c667a2083bd3802
Lucasaraujo6 <80993122+Lucasaraujo6@users.noreply.github.com>
2022-04-05 09:20:27 -0300

    feat: posts' comments on
    Os comentários já aparecem nos posts de acordo com o ID

--- js/details.js
+++ js/details.js
@@ -1,4 +1,5 @@
 const id = new URLSearchParams(window.location.search).get("id");
+
 const blogTitle_h1 = document.querySelector("h1");
 const blogBody_p = document.getElementById("blogBody");
 const back_button = document.querySelector("button");
@@ -6,6 +7,7 @@ const blogLikes_small = document.getElementById("likes");
 const delete_btn = document.getElementById("delete");
 
 // const console_button = document.getElementById("console");
+const coments_div = document.getElementById("comments");
 
 const renderPosts = async () => {
     const url = "http://localhost:3000/posts/" + id;
@@ -25,7 +27,8 @@ const likeBlog = async () => {
         method: "PATCH",
         body: JSON.stringify({ likes: parseInt(blogLikes_small.innerHTML) + 1 }),
         headers: { "Content-Type": "application/json" },
-    })
+    });
+    
 }
 
 const deletePost = async () => {
@@ -38,6 +41,37 @@ const deletePost = async () => {
         window.location.replace("/");
     };
 }
+
+const renderComments = async () => {
+  const url = "http://localhost:3000/comments";
+  const res = await fetch(url).then((response) => response.json());
+  const postComments = res.filter((item) => item.postId === parseInt(id));
+
+  let template = "";
+
+  console.log(postComments);
+  postComments.forEach(
+    (comment) =>
+      (template += `
+  <div class="blogComments">
+        <div class="blogHeader">
+          <div class="author">
+            <img src="rock.png" alt="" />
+            <div>
+              <p>author:</p>
+              <h2>${comment.author}</h2>
+            </div>
+          </div>
+          <button class="commentLikes">Likes : <small> ${comment.likes}</small></button>
+        </div>
+        <p class="comment">
+          ${comment.body}
+        </p>
+      </div>`)
+  );
+
+  comments_div.innerHTML = template;
+};
 
 
 delete_btn.addEventListener('click', () => deletePost());
