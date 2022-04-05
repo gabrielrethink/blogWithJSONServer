3b72a16f16184f145efc257526f9f3ba19749144
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-04-05 15:18:39 -0300

    feat: like comment, new comment

--- js/details.js
+++ js/details.js
@@ -8,6 +8,8 @@ const delete_btn = document.getElementById("delete");
 const blogComments = document.querySelector("blogComments");
 const comments_div = document.getElementById("comments");
 
+const formComment= document.querySelector("form");
+
 
 const renderPosts= async () => {
     const url = "http://localhost:3000/posts/" + id;
@@ -33,8 +35,8 @@ const likeBlog = async () => {
         
         console.log(res);
         blogLikes_small.innerHTML = res.likes;
-    };
-    
+};
+
 const deleteBlog = async () => {
     const url =" http://localhost:3000/posts/" + id;
     validation = confirm("Você tem certeza que deseja deletar esse Blog?");
@@ -52,50 +54,92 @@ const renderComment = async () => {
     const url =" http://localhost:3000/comments/";
     const res = await fetch(url).then((response) => response.json());
     const postComments = res.filter((item) => item.postId === parseInt(id));
+    console.log(postComments)
 
     let template = "";
-
+    
     postComments.forEach((comments) => template += `
     <div class="blogComments">
-            <div class="commentbody">
-                <div class="blogHeader">
-                    <!-- author, authorAvatar -->
-                    <div class="author">
-                        <img src="/icons/paper.png">
-                        <div>
-                            <p>author:</p>
-                            <h2>${comments.author}</h2>
-                        </div>
-                    </div>
-                    <!-- likes -->
-                    <button class="commentLikes">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="heart"
-                            viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-                        </svg>
-                    <small>${comments.likes} </small> 
-                    <p class="likeslabel"> likes </p>
-
-                </button>
-            </div>
-            <!-- body -->
-            <p id="bodyComment" class="comment">
-                ${comments.body}
-            </p>
-        </div>
-        </div>
-
+    <div class="commentbody">
+    <div class="blogHeader">
+    <!-- author, authorAvatar -->
+    <div class="author">
+    <img src="${comments.authorImg ?? "./icons/paper.png"}">
+    <div>
+    <p>author:</p>
+    <h2>${comments.author}</h2>
+    </div>
+    </div>
+    <!-- likes -->
+    <button id="buttonComment" class="commentsLikes" onCLick="onClickLikeButton(${comments.id}, ${comments.likes})">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="heart"
+    viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+    </svg>
+    <p class="likeslabel"> 
+    <small id="likesCommentSmall">${comments.likes} </small> 
+    likes </p>
+    
+    </button>
+    </div>
+    <!-- body -->
+    <p id="bodyComment" class="comment">
+    ${comments.body}
+    </p>
+    </div>
+    </div>
+    
     `)
-
+    
     comments_div.innerHTML = template;
 }
+const onClickLikeButton = async (idComment, likes) => {
+    const url =" http://localhost:3000/comments/" + idComment;
 
+    const res = await fetch(url, {
+        method: "PATCH",
+        body: JSON.stringify({likes: parseInt(likes) + 1}),
+        headers: {"Content-Type": "application/json"}
+    })
+
+        // console.log(res);
+};
+
+const enviarComentario = async (e) => {
+    e.preventDefault();
+    
+    const makeImg = () => {
+        const author = formComment.author.value.split(" ");
+        console.log(author);
+        if(author.length >= 2){
+            return `${author[0]}+${author[1]}`;
+        }
+        return author[0];
+    };
+
+    const comment ={
+        author: formComment.author.value,
+        body: formComment.body.value,
+        likes: 0,
+        postId: parseInt(id),
+        authorImg: `https://ui-avatars.com/api/?name=${makeImg()}&rounded=true`
+    };
+
+    const url = "http://localhost:3000/comments/";
+    await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(comment),
+        headers: {"Content-Type": "application/json"}
+    });
+}
 
 delete_btn.addEventListener("click", () => deleteBlog());
 blogLikes_small.addEventListener("click", () => likeBlog());
 back_button.addEventListener("click", () => window.location.replace("/"));
+// formComment.addEventListener("click", () => enviarComentario());
+formComment.addEventListener("submit", enviarComentario);
 window.addEventListener(
     "DOMContentLoaded",
     () => renderPosts(),
     renderComment()
-);
+    );
