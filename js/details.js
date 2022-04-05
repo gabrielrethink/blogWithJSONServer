e7f85fe2b165411fdf733fcc89eccce0eb6cb525
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-04-05 12:29:17 -0300

    feat: newComment, likeComment

--- js/details.js
+++ js/details.js
@@ -1,4 +1,4 @@
-const id = new URLSearchParams(window.location.search).get("id");
+const idPosts = new URLSearchParams(window.location.search).get("id");
 
 const blogTitle_h1 = document.querySelector("h1");
 const blogBody_p = document.getElementById("blogBody");
@@ -6,11 +6,12 @@ const back_button = document.getElementById("back");
 const blogLikes_small = document.getElementById("likes");
 const blogLikes_button = document.getElementById("likesButton");
 const delete_btn = document.getElementById("delete");
-
 const comments_div = document.getElementById("comments");
+const commentLikes_Btn = document.querySelectorAll(".commentLikes");
+const newComment_form = document.querySelector("form");
 
 const renderShowPost = async () => {
-  const url = "http://localhost:3000/posts/" + id;
+  const url = "http://localhost:3000/posts/" + idPosts;
   const res = await fetch(url).then((response) => response.json());
 
   blogTitle_h1.innerHTML = res.title;
@@ -19,7 +20,7 @@ const renderShowPost = async () => {
 };
 
 const likeBlog = async () => {
-  const url = "http://localhost:3000/posts/" + id;
+  const url = "http://localhost:3000/posts/" + idPosts;
 
   await fetch(url, {
     method: "PATCH",
@@ -31,7 +32,7 @@ const likeBlog = async () => {
 };
 
 const deleteBlog = async () => {
-  const url = "http://localhost:3000/posts/" + id;
+  const url = "http://localhost:3000/posts/" + idPosts;
   const validation = confirm("Voce tem certeza que deseja deletar esse Blog?");
   console.log(validation);
   if (validation) {
@@ -45,32 +46,80 @@ const deleteBlog = async () => {
 const renderComments = async () => {
   const url = "http://localhost:3000/comments";
   const res = await fetch(url).then((response) => response.json());
-  const postComments = res.filter((item) => item.postId === parseInt(id));
+
+  const postComments = res.filter((item) => item.postId === parseInt(idPosts));
 
   let template = "";
 
   postComments.forEach(
     (comment) =>
-      (template += `
-  <div class="blogComments">
+      (template += `<div class="blogComments">
         <div class="blogHeader">
           <div class="author">
-            <img src="rock.png" alt="" />
+            <img src="${comment.authorImg ?? "rock.png"}"/>
             <div>
               <p>author:</p>
               <h2>${comment.author}</h2>
             </div>
           </div>
-          <button class="commentLikes">Likes : <small> ${comment.likes}</small></button>
+          <button class="commentLikes" onClick="onClickLikeButton(${
+            comment.id
+          })">Likes : <small id="comment${comment.id}"> ${
+        comment.likes
+      }</small></button>
         </div>
         <p class="comment">
           ${comment.body}
         </p>
       </div>`)
   );
-
   comments_div.innerHTML = template;
 };
+
+const onClickLikeButton = async (idComment) => {
+  const like_btn = document.getElementById(`comment${idComment}`);
+
+  const url = "http://localhost:3000/comments/" + idComment;
+
+  await fetch(url, {
+    method: "PATCH",
+    body: JSON.stringify({ likes: parseInt(like_btn.innerHTML) + 1 }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  like_btn.innerHTML = parseInt(like_btn.innerHTML) + 1;
+};
+
+const newComment = async () => {
+  const makeImg = () => {
+    const author = newComment_form.author.value.split(" ");
+    console.log(author);
+    if (author.length === 2) {
+      return `${author[0]}+${author[1]}`;
+    }
+    return author[0];
+  };
+
+  const comment = {
+    author: newComment_form.author.value,
+    body: newComment_form.body.value,
+    postId: parseInt(idPosts),
+    likes: 0,
+    authorImg: `https://ui-avatars.com/api/?name=${makeImg()}&rounded=true`,
+  };
+  const url = "http://localhost:3000/comments/";
+
+  await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+newComment_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  newComment();
+});
 
 delete_btn.addEventListener("click", () => deleteBlog());
 
