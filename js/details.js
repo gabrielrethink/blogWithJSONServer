24d0a7a5a8c21133000095b05fa012cb3a3da0e6
Lucasaraujo6 <80993122+Lucasaraujo6@users.noreply.github.com>
2022-04-05 14:01:30 -0300

    feat: new comment

--- js/details.js
+++ js/details.js
@@ -6,7 +6,8 @@ const back_button = document.querySelector("button");
 const blogLikes_small = document.getElementById("likes");
 const delete_btn = document.getElementById("delete");
 
-// const console_button = document.getElementById("console");
+const formHtml_form = document.getElementById("commentForm");
+
 const comments_div = document.getElementById("comments");
 
 const renderPosts = async () => {
@@ -56,13 +57,13 @@ const renderComments = async () => {
       <div class="blogComments">
             <div class="blogHeader">
               <div class="author">
-                <img src="rock.png" alt="" />
+                <img src="${comment.avatar}" alt="" />
                 <div>
                   <p>author:</p>
                   <h2>${comment.author}</h2>
                 </div>
               </div>
-              <button class="commentLikes">Likes : <small> ${comment.likes}</small></button>
+              <button class="commentLikes" onClick="onClickLikeButton(${comment.id})">Likes: <small id="comment${comment.id}"> ${comment.likes}</small></button>
             </div>
             <p class="comment">
               ${comment.body}
@@ -71,9 +72,47 @@ const renderComments = async () => {
   );
 
   comments_div.innerHTML = template;
+
 };
 
 
+const onClickLikeButton = async (ide) => {
+  const like_btn = document.getElementById(`comment${ide}`);
+  const url = "http://localhost:3000/comments/" + ide;
+
+  await fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify({ likes: parseInt(like_btn.innerHTML) + 1 }),
+      headers: { "Content-Type": "application/json" },
+  });
+  console.log(like_btn.innerHTML);
+}
+
+
+const creatComment = async (e) => {
+  e.preventDefault();
+  console.log("to aqui");
+  const comment = {
+    likes: 0,
+    body: formHtml_form.body.value,
+    postId: parseInt(id),
+    avatar: makeAvatar(),
+    author: formHtml_form.author.value,
+  };
+  const url = "http://localhost:3000/comments";
+  await fetch(url, { 
+      method: "POST", 
+      body: JSON.stringify(comment),
+      headers: { "Content-Type": "application/json"},
+  });
+};
+function makeAvatar () { 
+  const text = formHtml_form.author.replace(" ", "+");
+  console.log(text);
+  return text;
+};
+
+formHtml_form.addEventListener("submit", (e) => creatComment(e));
 delete_btn.addEventListener('click', () => deletePost());
 blogLikes_small.addEventListener('click', () => likeBlog());
 back_button.addEventListener('click', () => window.location.replace("/"));
